4f106c29989589a2573f685886ad7de0d06e7b05
luisrojas07 <luisrojasm7@gmail.com>
2026-03-10 21:32:41 -0400

    deploy2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
+  // lrojas
   apiUrl: 'https://maneja-tus-gastos-api-7xxn.onrender.com',
 };
 
